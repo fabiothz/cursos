c00639babd04faf27fdb7942800e497d591df5b9
Fabio Thomaz <fabio@laddertecnologia.com.br>
2020-10-24 16:12:28 -0300

    Mutations with Inputs

--- GraphQL/graphqlcriando-apis-profissionais-e-flexiveis/mutations/resolvers/Mutation.js
+++ GraphQL/graphqlcriando-apis-profissionais-e-flexiveis/mutations/resolvers/Mutation.js
@@ -1,10 +1,21 @@
 const { usuarios, proximoId } = require('../data/db');
 
+function indiceUsuario(filtro) {
+    if (!filtro) return -1;
+    const { id, email } = filtro;
+    if (id) {
+        return usuarios.findIndex(u => u.id === id);
+    } else if (email) {
+        return usuarios.findIndex(u => u.email === email);
+    }
+    return -1;
+}
+
 module.exports = {
     // { nome, email, idade }
-    novoUsuario(_, args) {
+novoUsuario(_, { dados }) {
 
-        const emailExiste = usuarios.some(u => u.email == args.email);
+        const emailExiste = usuarios.some(u => u.email == dados.email);
 
         if (emailExiste) {
             throw new Error('E-mail cadastrado!');
@@ -12,7 +23,7 @@ module.exports = {
 
         const novo = {
             id: proximoId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO'
         };
@@ -20,19 +31,19 @@ module.exports = {
         usuarios.push(novo);
         return novo;
     },
-    excluirUsuario(_, { id }) {
-        const i = usuarios.findIndex(u => u.id === id);
+    excluirUsuario(_, { filtro }) {
+        const i = indiceUsuario(filtro);
         if (i < 0) return null;
         const excluidos = usuarios.splice(i, 1);
         return excluidos ? excluidos[0] : null;        
     },
-    alterarUsuario(_, args) {
-        const i = usuarios.findIndex(u => u.id === args.id);
+    alterarUsuario(_, { filtro, dados }) {
+        const i = indiceUsuario(filtro);
         if (i < 0) return null;
 
         const usuario = {
             ...usuarios[i],
-            ...args
+            ...dados
         }
 
         usuarios.splice(i, 1, usuario);
